e738972b4b9f6c1936327b64769349ff6d8e9db8
jogretoco88 <jogretoco88@gmail.com>
2026-05-25 22:14:45 -0600

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -2,19 +2,17 @@ import requests
 import json
 import os
 
-# Tu API Key está guardada en los "Secrets" de GitHub
-api_key = os.environ['API_FOOTBALL_KEY']
+api_key = os.environ.get('API_FOOTBALL_KEY')
 
 url = "https://v3.football.api-sports.io/fixtures?live=all"
+
 headers = {
-    "x-rapidapi-key": api_key,
-    "x-rapidapi-host": "v3.football.api-sports.io"
+    'x-rapidapi-key': api_key,
+    'x-rapidapi-host': 'v3.football.api-sports.io'
 }
 
 response = requests.get(url, headers=headers)
 data = response.json()
 
-# Guardamos los resultados en un archivo data.json
 with open('data.json', 'w') as f:
-    json.dump(data, f)
-  
+    json.dump(data, f, indent=4)
