4766266b32c41bbb5dc2d71bb990b0c42e315ad9
jogretoco88 <jogretoco88@gmail.com>
2026-05-25 21:47:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ fetch('data.json')
     .then(response => response.json())
     .then(data => {
         const contenedor = document.getElementById('resultados');
-        contenedor.innerHTML = JSON.stringify(data, null, 2); // Esto mostrará los datos crudos para empezar
+        contenedor.innerHTML = ''; // Limpiamos "Cargando..."
+
+        // Suponiendo que la API devuelve los partidos en data.response
+        data.response.forEach(partido => {
+            const div = document.createElement('div');
+            div.className = 'partido-card';
+            div.innerHTML = `
+                <span class="equipo">${partido.teams.home.name}</span>
+                <span class="marcador">${partido.goals.home} - ${partido.goals.away}</span>
+                <span class="equipo">${partido.teams.away.name}</span>
+            `;
+            contenedor.appendChild(div);
+        });
     });
