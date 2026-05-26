f3ff2d161371055f2fe42626c701eb6c9d25ba1e
jogretoco88 <jogretoco88@gmail.com>
2026-05-25 22:25:13 -0600

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -2,16 +2,21 @@ import requests
 import json
 import os
 
+# Usamos os.environ.get para acceder al secreto
 api_key = os.environ.get('API_FOOTBALL_KEY')
 
-url = "https://v3.football.api-sports.io/fixtures?live=all"
+if not api_key:
+    print("¡ERROR! La variable API_FOOTBALL_KEY no se encontró.")
+    exit(1)
 
+url = "https://v3.football.api-sports.io/fixtures?live=all"
 headers = {
     'x-rapidapi-key': api_key,
     'x-rapidapi-host': 'v3.football.api-sports.io'
 }
 
 response = requests.get(url, headers=headers)
+print(f"Código de respuesta: {response.status_code}")
 data = response.json()
 
 with open('data.json', 'w') as f:
