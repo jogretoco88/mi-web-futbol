b354b6f290c221f8f222da3f12b48868f5c49022
jogretoco88 <jogretoco88@gmail.com>
2026-05-25 22:50:23 -0600

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -7,8 +7,8 @@ api_key = os.environ.get('API_FOOTBALL_KEY')
 
 url = "https://v3.football.api-sports.io/fixtures?live=all"
 headers = {
-    "x-rapidapi-key": api_key,
-    "x-rapidapi-host": "v3.football.api-sports.io"
+    'x-rapidapi-key': api_key,
+    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
 }
 
 response = requests.get(url, headers=headers)
